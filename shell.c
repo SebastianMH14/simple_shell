e241394f7123d80932f710a7cec37fb7714db964
jdcera4 <jdcera4@gmail.com>
2021-04-11 16:58:08 +0000

    main

--- shell.c
+++ shell.c
@@ -3,18 +3,22 @@
 int main(void)
 {
      char *buff = NULL;
-     size_t readed, number = 0;
+     size_t readed, number = 0, b = EOF;
 
      while (1)
      {
-        write(1, "$ ", 2);
-        readed = getline(&buff, &number, stdin);
-        buff[readed -1] = 0;
-        execute(buff);
-        if (readed == EOF)
+        
+        if (readed != b)
+        {
+            write(1, "$ ", 2);
+            readed = getline(&buff, &number, stdin);
+            buff[readed -1] = 0;
+            token(buff);
+        }
+        else
          {
              free(buff);
-             return (-1);
+             exit(127);
          }
      }
      free(buff);
