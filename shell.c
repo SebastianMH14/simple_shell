4df81a80de2e6d5b6a44e66a0aada48df7bd62bf
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-12 18:42:33 +0000

    add description and betty fix

--- shell.c
+++ shell.c
@@ -1,26 +1,33 @@
 #include "shell.h"
 
+/**
+ *main - loop infinito
+ *
+ *
+ *Return: 0
+ */
+
 int main(void)
 {
-     char *buff = NULL;
-     size_t readed, number = 0, b = EOF;
+	char *buff = NULL;
+	size_t readed, number = 0, b = EOF;
 
-     while (1)
-     {
-        
-        if (readed != b)
-        {
-            write(1, "$ ", 2);
-            readed = getline(&buff, &number, stdin);
-            buff[readed -1] = 0;
-            token(buff);
-        }
-        else
-         {
-             free(buff);
-             exit(127);
-         }
-     }
-     free(buff);
-     return (0);
+	while (1)
+	{
+
+		if (readed != b)
+		{
+			write(1, "$ ", 2);
+			readed = getline(&buff, &number, stdin);
+			buff[readed - 1] = 0;
+			token(buff);
+		}
+		else
+		{
+			free(buff);
+			exit(127);
+		}
+	}
+	free(buff);
+	return (0);
 }
