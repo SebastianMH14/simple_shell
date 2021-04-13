c30204068676de29ccd53fb4a06ad36aebb97280
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-12 21:50:13 -0500

    cisfun

--- shell.c
+++ shell.c
@@ -1,34 +1,26 @@
 #include "shell.h"
-#include <unistd.h>
 
-/**
- *interactive - function for interactive mode
- *
- *
- *Return: 0
- */
-
-int interactive(void)
+int main(void)
 {
-	char *buff = NULL;
-	size_t readed, number = 0, b = EOF;
+     char *buff = NULL;
+     size_t readed, number = 0, b = EOF;
 
-	while (1)
-	{
+     while (1)
+     {
 
-		if (readed != b)
-		{
-			write(1, "$ ", 2);
-			readed = getline(&buff, &number, stdin);
-			buff[readed - 1] = 0;
-			token(buff);
-		}
-		else
-		{
-			free(buff);
-			exit(127);
-		}
-	}
-	free(buff);
-	return (0);
+        if (readed != b)
+        {
+            write(1, "#cisfun$ ", 2);
+            readed = getline(&buff, &number, stdin);
+            buff[readed -1] = 0;
+            token(buff);
+        }
+        else
+         {
+             free(buff);
+             exit(127);
+         }
+     }
+     free(buff);
+     return (0);
 }
