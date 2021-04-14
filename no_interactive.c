ff1a898fe82a5dcb4d163f06c49b596104eec48f
jdcera4 <jdcera4@gmail.com>
2021-04-14 03:10:13 +0000

    prompt

--- no_interactive.c
+++ no_interactive.c
@@ -7,9 +7,11 @@
 */
 void no_interactive(void)
 {
+	ssize_t readed;
 	char *buff = NULL;
-	size_t number, readed;
+	size_t number;
 
+	write(1, "#cisfun$ ", 9);
 	readed = getline(&buff, &number, stdin);
 	buff[readed - 1] = 0;
 	token(buff);
